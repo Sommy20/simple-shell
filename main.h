7cb0206c3c2061a755f05cd224d2f3eb2c2511be
Sommy20 <chi5chisom20@gmail.com>
2022-05-16 07:35:40 -0700

    main.h for this simple shell

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct string_array
+{
+    char string_inputs;
+};
+
+void display_prompt(void);
+void display_newline();
+
+
+/**
+ * string libraries
+ */
+
+
+
+#endif
